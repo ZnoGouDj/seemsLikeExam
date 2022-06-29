a05c472f254d74a465cf998e1b0d5f245869d37f
ZnoGouDj <znogoud3@gmail.com>
2022-06-29 22:03:00 +0200

    Active btns

--- src/App.vue
+++ src/App.vue
@@ -36,16 +36,6 @@ export default {
       currentTopic: 'click 👇 to start ',
       isStarted: false,
 
-      chevrons: [
-        {
-          name: 'up',
-          path: 'M416 352c-8.188 0-16.38-3.125-22.62-9.375L224 173.3l-169.4 169.4c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25C432.4 348.9 424.2 352 416 352z',
-        },
-        {
-          name: 'down',
-          path: 'M224 416c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L224 338.8l169.4-169.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-192 192C240.4 412.9 232.2 416 224 416z',
-        },
-      ],
       topics: [
         {
           id: 0,
@@ -83,11 +73,10 @@ export default {
   methods: {
     changeFilter(topic) {
       console.log(topic);
-      this.filteredTopics = topic;
-      this.stopTimer();
-      // this.currentTopics = this.fetchedTopics.filter(el => el)
-      this.fetchTopics();
-      this.isStarted = false;
+      // this.filteredTopics = topic;
+      // this.stopTimer();
+      // this.fetchTopics();
+      // this.isStarted = false;
     },
     setTopic() {
       let rnd = Math.floor(Math.random() * this.fetchedTopics.length);
--- src/components/UI/SelectButtons.vue
+++ src/components/UI/SelectButtons.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="btn-container">
-    <button v-for="image in images" :key="image.src" @click="onChange(image.alt)">
+    <button
+      v-for="image in images"
+      :key="image.src"
+      @click="onChange(image.alt)"
+      :class="{ active: image.isActive }"
+    >
       <img :src="image.src" :alt="image.alt" />
     </button>
   </div>
@@ -17,17 +22,20 @@ export default {
   data() {
     return {
       images: [
-        { src: html, alt: 'html/css' },
-        { src: js, alt: 'javascript' },
-        { src: react, alt: 'react' },
-        { src: redux, alt: 'redux' },
-        { src: ts, alt: 'typescript' },
+        { src: html, alt: 'html/css', isActive: false },
+        { src: js, alt: 'javascript', isActive: false },
+        { src: react, alt: 'react', isActive: false },
+        { src: redux, alt: 'redux', isActive: false },
+        { src: ts, alt: 'typescript', isActive: false },
       ],
     };
   },
   name: 'select-buttons',
   methods: {
     onChange(name) {
+      let current = this.images.find(el => el.alt === name);
+      this.images.forEach(el => (el.isActive = false));
+      current.isActive = true;
       this.$emit('onChange', name);
     },
   },
@@ -42,18 +50,24 @@ export default {
 button {
   border-radius: 8px;
   transition: 0.5s all;
-  width: 84px;
-  height: 84px;
+  width: 74px;
+  height: 74px;
   background: black;
+  margin: 0 10px;
 
   &:hover {
     transform: scale(1.1);
   }
+
+  &.active {
+    transform: scale(1.5);
+    box-shadow: 0px 0px 8px white;
+  }
 }
 
 img {
-  width: 80px;
-  height: 80px;
+  width: 70px;
+  height: 70px;
   background: white;
   border-radius: 8px;
 }
